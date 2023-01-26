4bf7013511aef7bbc3510a4347fb195c907aac78
vicente_nascimento <vicente19981@live.com>
2023-01-26 13:30:53 -0300

    feat: ensure CreateCandidateUseCase create a candidate

---
+++ src/candidate/usecase/create/create.candidate.usecase.ts
@@ -0,0 +1,39 @@
+import Candidate from '../../../candidate/domain/entity/candidate.entity';
+import { CandidateRepositoryInterface } from '../../../candidate/domain/repository/candidate.repository.interface';
+import Techs from '../../../candidate/domain/value-object/techs-value-object';
+import { Create } from './create.dto';
+
+export default class CreateCandidateUseCase {
+  constructor(
+    private readonly candidateRepository: CandidateRepositoryInterface,
+  ) {}
+
+  async execute(input: Create.Input): Promise<Create.Output> {
+    const candidate = new Candidate({
+      email: input.email,
+      name: input.name,
+      phone: input.phone,
+      github: input.github,
+      image: input.image,
+      techs: input.techs.map(
+        (tech) =>
+          new Techs({ knowledge_level: tech.knowledge_level, tech: tech.tech }),
+      ),
+    });
+
+    await this.candidateRepository.create(candidate);
+
+    return {
+      id: candidate.id,
+      email: candidate.email,
+      github: candidate.github,
+      image: candidate.image,
+      name: candidate.name,
+      phone: candidate.phone,
+      techs: candidate.techs.map((tech) => ({
+        knowledge_level: tech.knowledge_level,
+        tech: tech.tech,
+      })),
+    };
+  }
+}
