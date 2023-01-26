6f3108cc7bededbf945c892a004fec6ce8ffc0ff
vicente_nascimento <vicente19981@live.com>
2023-01-26 13:30:12 -0300

    feat: create candidate repository interface

---
+++ src/candidate/domain/repository/candidate.repository.interface.ts
@@ -0,0 +1,4 @@
+import Repository from 'src/@shared/repository/repository-interface';
+import Candidate from '../entity/candidate.entity';
+
+export type CandidateRepositoryInterface = Repository<Candidate>;
