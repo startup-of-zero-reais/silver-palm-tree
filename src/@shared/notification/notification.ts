48bc6945a3cded1ac8b837f6ef4152137d6da287
vicente_nascimento <vicente19981@live.com>
2023-01-26 12:04:15 -0300

    feat: create a notification to throw error

---
+++ src/@shared/notification/notification.ts
@@ -0,0 +1,32 @@
+export type NotificationErrorProps = {
+  message: string;
+  context: string;
+};
+
+export default class Notification {
+  private errors: NotificationErrorProps[] = [];
+
+  addError(error: NotificationErrorProps): void {
+    this.errors.push(error);
+  }
+
+  hasError(): boolean {
+    return this.errors.length > 0;
+  }
+
+  getErrors(): NotificationErrorProps[] {
+    return this.errors;
+  }
+
+  messages(context?: string): string {
+    let message = '';
+
+    this.errors.forEach((error) => {
+      if (context === undefined || error.context === context) {
+        message += `${error.context}: ${error.message}`;
+      }
+    });
+
+    return message;
+  }
+}
