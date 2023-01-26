36a2af2a10f7b9761525a109a568087f9f9fdf96
vicente_nascimento <vicente19981@live.com>
2023-01-26 13:29:13 -0300

    feat: create repository default interface

---
+++ src/@shared/repository/repository-interface.ts
@@ -0,0 +1,6 @@
+export default interface Repository<T> {
+  create(entity: T): Promise<void>;
+  update(entity: T): Promise<void>;
+  find(id: string): Promise<T>;
+  findAll(id: string): Promise<T[]>;
+}
