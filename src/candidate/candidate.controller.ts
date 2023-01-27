8ea27a8a86b3abb44c22dd62de80f9c88f37227a
vicente_nascimento <vicente19981@live.com>
2023-01-26 23:23:54 -0300

    feat: create controller candidate

--- src/candidate/candidate.controller.ts
+++ src/candidate/candidate.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Response } from '@nestjs/common';
+import CreateCandidateUseCase from './usecase/create/create.candidate.usecase';
+import { Create } from './usecase/create/create.dto';
+import { Response as eResponse } from 'express';
 
 @Controller('candidate')
-export class CandidateController {}
+export class CandidateController {
+  constructor(
+    private readonly createCandidateUseCase: CreateCandidateUseCase,
+  ) {}
+
+  @Post()
+  async create(
+    @Body() createCandidateDto: Create.Input,
+    @Response() response: eResponse,
+  ) {
+    try {
+      const output = await this.createCandidateUseCase.execute(
+        createCandidateDto,
+      );
+      return response.status(201).json(output);
+    } catch (error) {
+      return response.status(500).json(error);
+    }
+  }
+}
