a841a33a0ceda7c6bb2874edbbb9e5e31c5344ed
vicente_nascimento <vicente19981@live.com>
2023-01-26 23:20:27 -0300

    feat adding mongo config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CandidateModule } from './candidate/candidate.module';
 
 @Module({
-  imports: [CandidateModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://root:root@mongo:27017'),
+    CandidateModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
