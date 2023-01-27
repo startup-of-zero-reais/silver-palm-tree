33a94a4c4cdbc87f66a03100be601d71d7712fa9
vicente_nascimento <vicente19981@live.com>
2023-01-27 18:46:39 -0300

    feat: create pagination presenter infra

---
+++ src/candidate/infra/repository/presenter/pagination.presenter.spec.ts
@@ -0,0 +1,13 @@
+import PaginationPresenter from './pagination.presenter';
+
+describe('', () => {
+  it('should pagination presenter on items', () => {
+    const items = ['any_item1', 'any_item2', 'any_item3'];
+    const paginationPresenter = new PaginationPresenter(items, 2, 1, 3);
+
+    expect(paginationPresenter.total()).toBe(items.length);
+    expect(paginationPresenter.currentPage()).toBe(1);
+    expect(paginationPresenter.firstPage()).toBe(1);
+    expect(paginationPresenter.lastPage()).toBe(2);
+  });
+});
