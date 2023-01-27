22fc6be0f4a6a0b47ccee7da39f55612e804099b
vicente_nascimento <vicente19981@live.com>
2023-01-27 18:46:27 -0300

    refactor: change list dto

--- src/candidate/usecase/list/list.dto.ts
+++ src/candidate/usecase/list/list.dto.ts
@@ -1,15 +1,30 @@
 import { KnowledgeLevel } from '../../../candidate/domain/value-object/techs-value-object';
 
 export namespace List {
+  export type Input = {
+    page_size: number;
+    page: number;
+  };
   export type Output = {
-    id: string;
-    name: string;
-    email: string;
-    image: string;
-    phone: string;
-    techs?: {
-      knowledge_level: KnowledgeLevel;
-      tech: string;
+    data: {
+      id: string;
+      name: string;
+      email: string;
+      image: string;
+      phone: string;
+      techs?: {
+        knowledge_level: KnowledgeLevel;
+        tech: string;
+      }[];
+      createdAt: Date;
+      updatedAt: Date;
     }[];
-  }[];
+    meta: {
+      total: number;
+      lastPage: number;
+      firstPage: number;
+      currentPage: number;
+      perPage: number;
+    };
+  };
 }
