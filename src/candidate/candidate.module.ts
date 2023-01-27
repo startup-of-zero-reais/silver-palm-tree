4b83dc754b94c66c610c2beb0193da99669f044d
vicente_nascimento <vicente19981@live.com>
2023-01-26 23:21:53 -0300

    feat: create module candidate

--- src/candidate/candidate.module.ts
+++ src/candidate/candidate.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CandidateController } from './candidate.controller';
+import { CANDIDATE_PROVIDERS } from './candidate.provider';
+import {
+  Candidate,
+  CandidateSchema,
+} from './infra/repository/typeorm/candidate.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Candidate.name, schema: CandidateSchema },
+    ]),
+  ],
   controllers: [CandidateController],
+  providers: [
+    ...Object.values(CANDIDATE_PROVIDERS.REPOSITORIES),
+    ...Object.values(CANDIDATE_PROVIDERS.USE_CASES),
+  ],
 })
 export class CandidateModule {}
