7e2ea162fa9c79338e03a3929152b0414ba72c2d
vicente_nascimento <vicente19981@live.com>
2023-01-26 23:22:16 -0300

    feat: create candidate model

---
+++ src/candidate/infra/repository/typeorm/candidate.model.ts
@@ -0,0 +1,25 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Techs } from './techs.model';
+
+export type CandidateDocument = HydratedDocument<Candidate>;
+
+@Schema()
+export class Candidate {
+  @Prop({ type: mongoose.Types.ObjectId })
+  _id: string;
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  photo: string;
+
+  @Prop([Techs])
+  techs: Techs[];
+}
+
+export const CandidateSchema = SchemaFactory.createForClass(Candidate);
