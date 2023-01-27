fd12bd1ce2f01166fe25e59848703a50dea9ae5f
vicente_nascimento <vicente19981@live.com>
2023-01-27 18:45:05 -0300

    feat: adding createdAt and updatedAt

--- src/@shared/entity/entity.abstract.ts
+++ src/@shared/entity/entity.abstract.ts
@@ -1,14 +1,25 @@
+import { randomUUID } from 'crypto';
 import Notification from '../notification/notification';
 
 export default abstract class Entity {
   protected _id: string;
+  protected _createdAt: Date;
+  protected _updatedAt: Date;
   public notification: Notification;
 
-  constructor() {
+  constructor(id?: string, createdAt?: Date, updatedAt?: Date) {
     this.notification = new Notification();
+    this._id = id || randomUUID();
+    this._createdAt = createdAt ? createdAt : new Date();
+    this._updatedAt = updatedAt ? updatedAt : new Date();
   }
-
   get id(): string {
     return this._id;
+  }
+  get createdAt(): Date {
+    return this._createdAt;
+  }
+  get updatedAt(): Date {
+    return this._updatedAt;
   }
 }
