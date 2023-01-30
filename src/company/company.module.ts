7cad05964a6ee47be365c491792c0d890d09a527
vicente_nascimento <vicente19981@live.com>
2023-01-30 18:02:16 -0300

    feat: imports a modules

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CompanyController } from './company.controller';
 import { CompanyFacade } from './facade/company.facade';
+import { Company, CompanySchema } from './infra/repository/mongo/company.model';
+import { CompanyMongoRepository } from './infra/repository/mongo/company.repository';
+import { CreateCompanyUseCase } from './usecase/create.company.usecase';
 
 @Module({
-	providers: [CompanyFacade],
+	imports: [
+		MongooseModule.forFeature([
+			{ name: Company.name, schema: CompanySchema },
+		]),
+	],
+	providers: [
+		// FACADE
+		CompanyFacade,
+		// USECASE
+		CreateCompanyUseCase,
+		//REPOSITORY
+		CompanyMongoRepository,
+	],
 	controllers: [CompanyController],
 	exports: [CompanyFacade],
 })
