3c337b6318040c95848c61b31d76bedb07ddae5a
vicente_nascimento <vicente19981@live.com>
2023-01-30 16:46:59 -0300

    feat: create validator of company entity

---
+++ src/company/domain/validator/company.validator.ts
@@ -0,0 +1,41 @@
+import * as yup from 'yup';
+import ValidatorInterface from '@/@shared/validator/ValidatorInterface';
+import Company from '../entity/company.entity';
+import { validatorCNPJ } from '@/@shared/helper/validator.cnpj';
+
+export default class CompanyYupValidator
+	implements ValidatorInterface<Company>
+{
+	validate(entity: Company): void {
+		try {
+			yup.object()
+				.shape({
+					cnpj: yup
+						.string()
+						.test(
+							'test-invalid-cnpj',
+							'cnpj is invalid',
+							(cnpj: string) => {
+								return validatorCNPJ(cnpj);
+							},
+						)
+						.required(),
+					description: yup.string().required(),
+					logo: yup.string().url().required(),
+				})
+				.validateSync(
+					{
+						cnpj: entity.cnpj,
+						description: entity.description,
+						logo: entity.logo,
+					},
+					{ abortEarly: false },
+				);
+		} catch (errors) {
+			const e = errors as yup.ValidationError;
+			e.errors.forEach((error) => {
+				entity.notification.addError({ message: error });
+			});
+		}
+	}
+}
