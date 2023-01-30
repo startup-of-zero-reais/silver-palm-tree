75b530eee163a1f727935950df16d04e06c0e342
vicente_nascimento <vicente19981@live.com>
2023-01-30 19:59:42 -0300

    feat: adding find by cnpj

---
+++ src/company/usecase/find-by-cnpj/find-by-cnpj.usecase.ts
@@ -0,0 +1,22 @@
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import UseCaseInterface from '@/@shared/usecase/use-case.interface';
+import Company from '@/company/domain/entity/company.entity';
+import { CompanyRepositoryInterface } from '@/company/domain/repository/company.repository.interface';
+import { CompanyMongoRepository } from '@/company/infra/repository/mongo/company.repository';
+import { FindByCNPJCompanyInputDto } from './find-by-cnpj.dto';
+
+@Injectable()
+export class FindByCNPJ implements UseCaseInterface {
+	constructor(
+		@Inject(CompanyMongoRepository)
+		private readonly companyRepository: CompanyRepositoryInterface,
+	) {}
+
+	async execute(input: FindByCNPJCompanyInputDto): Promise<Company> {
+		const company = await this.companyRepository.findByCnpj(input.cnpj);
+
+		if (!company) throw new NotFoundException('Company not found');
+
+		return company;
+	}
+}
