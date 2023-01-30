b10142abaedf4738c9d8c6e74f64e1190513fa41
vicente_nascimento <vicente19981@live.com>
2023-01-30 18:19:36 -0300

    feat: create find dto of company

---
+++ src/company/usecase/find/find.dto.ts
@@ -0,0 +1,22 @@
+import { Exclude, Expose } from 'class-transformer';
+import { IsNotEmpty } from 'class-validator';
+
+export class FindCompanyInputDto {
+	@IsNotEmpty()
+	id: string;
+}
+
+@Exclude()
+export class FindCompanyOutputDto {
+	@Expose()
+	id: string;
+
+	@Expose()
+	logo: string;
+
+	@Expose()
+	cnpj: string;
+
+	@Expose()
+	description: string;
+}
