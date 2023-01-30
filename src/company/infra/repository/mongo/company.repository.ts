7ecdb42cbf80e49f30a889bed147a83bf91b3452
vicente_nascimento <vicente19981@live.com>
2023-01-30 18:02:01 -0300

    fix: typo

--- src/company/infra/repository/mongo/company.repository.ts
+++ src/company/infra/repository/mongo/company.repository.ts
@@ -10,25 +10,30 @@ import { Company, CompanyDocument } from './company.model';
 export class CompanyMongoRepository implements CompanyRepositoryInterface {
 	constructor(
 		@InjectModel(Company.name)
-		private candidateModel: Model<CompanyDocument>,
+		private companyModel: Model<CompanyDocument>,
 	) {}
+
 	async create(entity: CompanyEntity): Promise<void> {
-		await this.candidateModel.create({
+		await this.companyModel.create({
 			_id: entity.id,
 			cnpj: entity.cnpj,
 			logo: entity.logo,
 			description: entity.description,
 		});
 	}
+
 	update(entity: CompanyEntity): Promise<void> {
 		throw new Error('Method not implemented.');
 	}
+
 	find(id: string): Promise<CompanyEntity> {
 		throw new Error('Method not implemented.');
 	}
+
 	findByEmail(email: string): Promise<CompanyEntity> {
 		throw new Error('Method not implemented.');
 	}
+
 	paginate(
 		per_page: number,
 		page: number,
