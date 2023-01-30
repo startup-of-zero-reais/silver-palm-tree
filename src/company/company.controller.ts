b7c2a7e318fcd42f57a6912e20718f8d8204f154
vicente_nascimento <vicente19981@live.com>
2023-01-30 17:34:16 -0300

    feat: create company controller

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Response } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { Response as eResponse } from 'express';
+import { CreateCompanyUseCase } from './usecase/create.company.usecase';
+import {
+	CreateCompanyInputDto,
+	CreateCompanyOutputDto,
+} from './usecase/create.dto';
 
-@Controller('company')
-export class CompanyController {}
+@Controller('companies')
+export class CompanyController {
+	constructor(private readonly createCompanyUseCase: CreateCompanyUseCase) {}
+
+	@Post()
+	async create(
+		@Body() input: CreateCompanyInputDto,
+		@Response() response: eResponse,
+	) {
+		try {
+			const recruiter = await this.createCompanyUseCase.execute(input);
+			return response
+				.status(HttpStatus.CREATED)
+				.json(plainToClass(CreateCompanyOutputDto, recruiter));
+		} catch (e) {
+			return response
+				.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.json({ error: e.message });
+		}
+	}
+}
