1119f7a65052190de2fd7eebec6fbaf5a54dd0b4
vicente_nascimento <vicente19981@live.com>
2023-01-30 14:58:55 -0300

    test: ensure CreateRecruiterUseCase create a recruiter

---
+++ src/recruiter/usecase/create/create.recruiter.usecase.spec.ts
@@ -0,0 +1,91 @@
+import CompanyFacadeInterface from '@/company/facade/company.facade.interface';
+import {
+	Recruiter,
+	RecruiterRepositoryInterface,
+	Status,
+} from '@/recruiter/domain';
+import { CreateRecruiterUseCase } from './create.recruiter.usecase';
+
+const recruiter = new Recruiter({
+	company: { cnpj: 'any_cnpj', id: 'any_id' },
+	email: 'any_email',
+	password: 'any_password',
+	image: 'http://any_image.com',
+	name: 'any_name',
+	status: Status.ACTIVATED,
+});
+
+const input = {
+	name: 'any_name',
+	email: 'any_email',
+	image: 'http://image.com',
+	password: 'any_password',
+	cnpj: 'any_cnpj',
+};
+
+let MockRepository = (): RecruiterRepositoryInterface => {
+	return {
+		findByEmail: jest.fn().mockResolvedValue(undefined),
+		find: jest.fn(),
+		paginate: jest.fn(),
+		create: jest.fn(),
+		update: jest.fn(),
+	};
+};
+
+const MockCompanyFacade = (): CompanyFacadeInterface => {
+	return {
+		getByCNPJ: jest
+			.fn()
+			.mockReturnValue({ id: 'any_id', cnpj: 'any_cnpj' }),
+	};
+};
+
+describe('Unit test create recruiter usecase', () => {
+	it('Should create recruiter usecase', async () => {
+		const usecase = new CreateRecruiterUseCase(
+			MockRepository(),
+			MockCompanyFacade(),
+		);
+
+		const output = await usecase.execute(input);
+
+		expect(output.id).toBeDefined();
+		expect(output.name).toBe(input.name);
+		expect(output.email).toBe(input.email);
+		expect(output.image).toBe(input.image);
+		expect(output.password).not.toBe(input.password);
+	});
+
+	it('Should CreateRecruiterUseCase throws error if candidate has already exists', async () => {
+		const MockThrowRepository = (): RecruiterRepositoryInterface => ({
+			...MockRepository(),
+			findByEmail: jest.fn().mockResolvedValue(recruiter),
+		});
+
+		const usecase = new CreateRecruiterUseCase(
+			MockThrowRepository(),
+			MockCompanyFacade(),
+		);
+
+		await expect(usecase.execute(input)).rejects.toThrow(
+			'Recruiter with this email already exists',
+		);
+	});
+
+	it('Should CreateRecruiterUseCase throws error if company not found', async () => {
+		const MockThrowFacade = (): CompanyFacadeInterface => ({
+			...MockCompanyFacade(),
+			getByCNPJ: jest.fn().mockResolvedValue(undefined),
+		});
+
+		const usecase = new CreateRecruiterUseCase(
+			MockRepository(),
+			MockThrowFacade(),
+		);
+
+		await expect(usecase.execute(input)).rejects.toThrow(
+			'Company not found',
+		);
+	});
+});
