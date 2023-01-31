ca6f94e0fc76109a5e79e59f73bb76c0429fb03d
vicente_nascimento <vicente19981@live.com>
2023-01-30 23:20:51 -0300

    feat: enabling create company

--- src/company/facade/company.facade.interface.ts
+++ src/company/facade/company.facade.interface.ts
@@ -2,4 +2,5 @@ import Company from '../domain/entity/company.entity';
 
 export default interface CompanyFacadeInterface {
 	getByCNPJ(cnpj: string): Promise<Company>;
+	create(company: Company): Promise<Company>;
 }
