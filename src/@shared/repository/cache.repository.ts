fb7cd3529d65539d80a895d194e29ca6784c1ae0
Jean Molossi <jean.carlosmolossi@gmail.com>
2023-02-02 21:57:41 -0300

    feat: add memory cache

---
+++ src/@shared/repository/cache.repository.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+
+type ValueWithTTL<V = any> = {
+	value: V;
+	ttl: number;
+};
+
+const cache = new Map<string, ValueWithTTL>([]);
+
+@Injectable()
+export class CacheManager {
+	public async get<V>(key: string): Promise<V> {
+		return cache.get(key)?.value;
+	}
+
+	public async set<V>(key: string, value: V, ttl = 5) {
+		const endTTL = Date.now() + ttl * 1000;
+
+		cache.set(key, { value, ttl: endTTL });
+	}
+
+	public async reset() {
+		cache.clear();
+	}
+}
+
+export function cacheTicker() {
+	const timer = setInterval(() => {
+		for (const [key, item] of cache) {
+			if (Date.now() > item.ttl) {
+				cache.delete(key);
+				console.log(`key: ${key} deleted`);
+			}
+		}
+	}, 1000);
+
+	return () => {
+		clearInterval(timer);
+	};
+}
