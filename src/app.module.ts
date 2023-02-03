65cb0fa25c9b3b4e71659a2b9a9a21f6d16b73c9
Jean Molossi <jean.carlosmolossi@gmail.com>
2023-02-02 22:04:01 -0300

    feat: initiate cache manager

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MainExceptionFilter } from './@shared/exception-filter/http-exception.filter';
+import {
+	CacheManager,
+	cacheTicker,
+} from './@shared/repository/cache.repository';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -32,6 +36,18 @@ import { RecruiterModule } from './recruiter/recruiter.module';
 			useClass: MainExceptionFilter,
 		},
 		AppService,
+		CacheManager,
 	],
+	exports: [CacheManager],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit, OnModuleDestroy {
+	private cacheManager: () => void;
+
+	onModuleInit() {
+		this.cacheManager = cacheTicker();
+	}
+
+	onModuleDestroy() {
+		this.cacheManager();
+	}
+}
